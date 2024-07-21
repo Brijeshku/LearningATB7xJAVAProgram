2a9c6ecd5ebbba26f007db3c0c53e435ad9b2dec
Brijesh Kumar <bkumar9983@yahoo.in>
2024-07-21 09:17:15 +0530

    July 14 and 17 programs changes for leap years and fizzbuzz

---
+++ src/ex_20072024/Lab008.java
@@ -0,0 +1,27 @@
+package ex_20072024;
+
+public class Lab008 {
+
+        public static void main(String[] args) {
+            int year = 2024; // Specify the year to check
+
+            if (isLeapYear(year)) {
+                System.out.println(year + " is a leap year.");
+            } else {
+                System.out.println(year + " is not a leap year.");
+            }
+        }
+
+        public static boolean isLeapYear(int year) {
+            // Check if the year is a leap year
+            if (year % 4 == 0) {
+                if (year % 100 == 0) {
+                    return year % 400 == 0; // Divisible by 400
+                }
+                return true; // Divisible by 4 but not by 100
+            }
+            return false; // Not a leap year
+        }
+    }
+
+
